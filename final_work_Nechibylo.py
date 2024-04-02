40e0bf54d6a6f296f7dd7971e8a631e5c3e20478
Oleg <oilminer@mail.ru>
2024-04-02 17:53:10 +0500

    Добавил метод get_dummies

--- final_work_Nechibylo.py
+++ final_work_Nechibylo.py
@@ -1,8 +1,14 @@
-import pandas as pd
-import random
+# import pandas as pd
+from pandas import DataFrame, get_dummies    # Имрорт DataFrame
+import random       # Импорт random для рандомного заполнения DataFrame
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
-data.head()
-print(data)
+data = DataFrame({'whoAmI': lst})       # Заполнение DataFrame рандомными значениями поля 'whoAmI'
+#print(data.head(20))
+print(data)     # Выводим DataFrame полностью, для чистоты эксперимента
+print()
+print("Результат работы программы:")
+print()
+new_data = get_dummies(data['whoAmI'])
+print(new_data)
